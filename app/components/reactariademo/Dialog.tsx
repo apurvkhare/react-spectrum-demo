b905efe2a6958cd392270bc8de10646f2d83a30b
apurvkhare <apurvkhare1@gmail.com>
2023-06-30 22:53:32 +0530

    react aria demo complete

--- app/components/reactariademo/Dialog.tsx
+++ app/components/reactariademo/Dialog.tsx
@@ -1,23 +1,37 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Button } from './Button'
+import { FocusScope, OverlayContainer, useDialog, useModal, useOverlay } from 'react-aria'
 
 export const Dialog = props => {
     let { title, children, confirmLabel, onClose } = props
 
+    const dialogRef = useRef(null)
+    const { overlayProps } = useOverlay(props, dialogRef)
+    const { dialogProps, titleProps } = useDialog(props, dialogRef)
+    const { modalProps } = useModal()
+
     return (
         <div className='fixed inset-0 flex items-center justify-center z-100'>
             <div className='absolute inset-0 bg-gray-700 opacity-75 z-0' />
-            <div className='bg-white rounded-lg p-8 z-1 relative focus:outline-none'>
-                <h3 className='text-lg font-medium pb-2'>{title}</h3>
-                <div className='text-sm text-gray-600'>{children}</div>
-                <div className='pt-8 flex space-x-3 justify-end'>
-                    <Button onPress={onClose}>Cancel</Button>
-                    <Button onPress={onClose}>{confirmLabel}</Button>
+            <FocusScope contain autoFocus restoreFocus>
+                <div
+                    ref={dialogRef}
+                    {...overlayProps}
+                    {...dialogProps}
+                    {...modalProps}
+                    className='bg-white rounded-lg p-8 z-1 relative focus:outline-none'
+                >
+                    <h3 {...titleProps} className='text-lg font-medium pb-2'>{title}</h3>
+                    <div className='text-sm text-gray-600'>{children}</div>
+                    <div className='pt-8 flex space-x-3 justify-end'>
+                        <Button onPress={onClose}>Cancel</Button>
+                        <Button onPress={onClose}>{confirmLabel}</Button>
+                    </div>
                 </div>
-            </div>
+            </FocusScope>
         </div>
     )
 }
@@ -31,12 +45,12 @@ export function DialogTrigger({ label, children, ...otherProps }) {
                 {label}
             </Button>
             {isOpen && (
-                <Portal>
+                <OverlayContainer>
                     {React.cloneElement(children, {
                         isOpen,
                         onClose: () => setOpen(false),
                     })}
-                </Portal>
+                </OverlayContainer>
             )}
         </>
     )
